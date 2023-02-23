0c473748b8d70fd45fb74c9cbbd5239d2332c110
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-23 19:36:04 -0300

    free pointers

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 18:45:21 by gmasid            #+#    #+#             */
-/*   Updated: 2023/02/23 19:15:22 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/02/23 19:31:03 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,18 @@
 
 # include "../mlx/mlx.h"
 # include <stdio.h>
+# include <stdlib.h>
 
 typedef struct s_game
 {
 	void	*mlx;
 	void	*win;
 }			t_game;
+
+enum		e_event_code
+{
+	E_KEYPRESS = 2,
+	E_CLOSE_WINDOW = 17
+};
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,19 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 18:25:36 by gmasid            #+#    #+#             */
-/*   Updated: 2023/02/23 19:17:42 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/02/23 19:33:07 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+int	close_game(t_game *game)
+{
+	mlx_destroy_window(game->mlx, game->win);
+	free(game->mlx);
+	exit(0);
+	return (0);
+}
 
 int	main(void)
 {
@@ -18,5 +26,6 @@ int	main(void)
 
 	game.mlx = mlx_init();
 	game.win = mlx_new_window(game.mlx, 900, 500, "cub3D");
+	mlx_hook(game.win, E_CLOSE_WINDOW, 1L << 2, close_game, &game);
 	mlx_loop(game.mlx);
 }
