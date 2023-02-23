dd9f7649cdab38194c5783d354d8fdfc6b0a84c1
Guilhermo Masid <guilhermomasid@gmail.com>
2023-02-23 19:10:37 -0300

    fix norm

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,19 +6,19 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 18:45:21 by gmasid            #+#    #+#             */
-/*   Updated: 2023/02/23 18:57:05 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/02/23 19:10:12 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <stdio.h>
+# include <stdio.h>
 
 typedef struct s_game
 {
-  void *mlx;
-  void *win;
-} t_game;
+	void	*mlx;
+	void	*win;
+}			t_game;
 
 #endif
