6b02ec5d89189d3cb256a8466ded8443e9ca8bd9
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-04 16:15:30 -0300

    jesus christ i hate norminette

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,30 +6,29 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 18:45:21 by gmasid            #+#    #+#             */
-/*   Updated: 2023/03/03 15:20:26 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/03/04 16:14:23 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-# define CUB3D_H
+#define CUB3D_H
 
-# include "../includes/constants.h"
-# include "../mlx/mlx.h"
-# include <math.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct s_game
-{
-	void	*mlx;
-	void	*win;
-}			t_game;
+#include "../includes/constants.h"
+#include "../mlx/mlx.h"
 
-enum		e_event_code
-{
-	E_KEYPRESS = 2,
-	E_CLOSE_WINDOW = 17
+typedef struct s_game {
+  void *mlx;
+  void *win;
+} t_game;
+
+enum e_event_code {
+  E_KEYPRESS = 2,
+  E_CLOSE_WINDOW = 17
 };
 
 #endif
