35553ad90b895e69c5363bb681393a3dd7a58b4c
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-16 19:39:35 -0300

    init struct values

--- src/init.c
+++ src/init.c
@@ -6,20 +6,58 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 17:42:52 by gmasid            #+#    #+#             */
-/*   Updated: 2023/03/16 18:12:13 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/03/16 19:38:59 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	init_game(t_game *game)
+void	init_map(t_game *game)
 {
-	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
+	game->map.array = NULL;
+	game->map.height = 0;
+	game->map.width = 0;
+}
+
+void	init_ray(t_game *game)
+{
+	game->ray.angle = 0;
+	game->ray.cos = 0;
+	game->ray.hfov = 30;
+	game->ray.increment_angle = 2 * game->ray.hfov / WINDOW_WIDTH;
+	game->ray.limit = 11;
+	game->ray.precision = 50;
+	game->ray.sin = 0;
+	game->ray.x_pos = 0;
+	game->ray.y_pos = 0;
+}
+
+void	init_keys(t_game *game)
+{
 	game->keys.is_w_pressed = 0;
 	game->keys.is_a_pressed = 0;
 	game->keys.is_s_pressed = 0;
 	game->keys.is_d_pressed = 0;
 	game->keys.is_arrow_left_pressed = 0;
 	game->keys.is_arrow_right_pressed = 0;
+}
+
+void	init_player(t_game *game)
+{
+	game->player.dir = 0;
+	game->player.speed = 0.10;
+	game->player.x_pos = -1;
+	game->player.y_pos = -1;
+}
+
+void	init_game(t_game *game)
+{
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
+	init_map(game);
+	init_ray(game);
+	init_keys(game);
+	init_player(game);
+	game->frame.count = 0;
+	game->frame.rate = 30;
 }
