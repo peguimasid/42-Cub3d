ed03396e685edf5462f4251838066e2824a77f46
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-27 19:17:55 -0300

    create function to handle line and jump empty lines from the file

--- src/parse_map/main.c
+++ src/parse_map/main.c
@@ -6,24 +6,32 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:19:23 by gmasid            #+#    #+#             */
-/*   Updated: 2023/03/27 19:07:20 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/03/27 19:14:30 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
+void	handle_line(char *line, int count)
+{
+	printf("line = %s || count = %d\n", line, count);
+}
+
 void	parse_map(char *path, t_game *game)
 {
 	int		fd;
+	int		count;
 	char	*line;
 
 	fd = open(path, O_RDONLY);
+	count = 0;
 	line = get_next_line_trimmed(fd);
 	if (!line)
 		exit_app("You provide an empty file", game);
 	while (line)
 	{
-		printf("%s\n", line);
+		if (ft_strlen(line))
+			handle_line(line, count++);
 		free(line);
 		line = get_next_line_trimmed(fd);
 	}
