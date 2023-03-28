aae61a62ca423d7182ab83c20c008df8073691f3
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-28 16:53:48 -0300

    add close_game temporarily to prevent leaks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 18:25:36 by gmasid            #+#    #+#             */
-/*   Updated: 2023/03/27 18:23:16 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/03/28 16:53:02 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ int	main(int argc, char **argv)
 		return (1);
 	init_cub(&game);
 	parse_map(argv[1], &game);
+	// TODO: Remove this 'close_game' function from here
+	close_game(&game);
 }
