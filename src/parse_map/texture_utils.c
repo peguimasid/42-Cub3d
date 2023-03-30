a83870398858651efd555577c39c6c23696d98de
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-30 16:36:31 -0300

    make is_valid_key function use utils function to better reading

--- src/parse_map/texture_utils.c
+++ src/parse_map/texture_utils.c
@@ -6,45 +6,11 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:12:19 by gmasid            #+#    #+#             */
-/*   Updated: 2023/03/29 19:59:43 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/03/30 16:34:01 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
-
-int	is_valid_key(char *key)
-{
-	if (ft_strncmp(key, "NO", 2) == 0 && ft_strlen(key) == 2)
-		return (1);
-	if (ft_strncmp(key, "SO", 2) == 0 && ft_strlen(key) == 2)
-		return (1);
-	if (ft_strncmp(key, "WE", 2) == 0 && ft_strlen(key) == 2)
-		return (1);
-	if (ft_strncmp(key, "EA", 2) == 0 && ft_strlen(key) == 2)
-		return (1);
-	if (ft_strncmp(key, "F", 1) == 0 && ft_strlen(key) == 1)
-		return (1);
-	if (ft_strncmp(key, "C", 1) == 0 && ft_strlen(key) == 1)
-		return (1);
-	return (0);
-}
-
-int	is_duplicate_key(char *key, t_game *game)
-{
-	if (ft_strncmp(key, "NO", 2) == 0 && game->textures.north)
-		return (1);
-	if (ft_strncmp(key, "SO", 2) == 0 && game->textures.south)
-		return (1);
-	if (ft_strncmp(key, "WE", 2) == 0 && game->textures.west)
-		return (1);
-	if (ft_strncmp(key, "EA", 2) == 0 && game->textures.east)
-		return (1);
-	if (ft_strncmp(key, "F", 1) == 0 && game->textures.floor != -1)
-		return (1);
-	if (ft_strncmp(key, "C", 1) == 0 && game->textures.ceiling != -1)
-		return (1);
-	return (0);
-}
 
 int	is_color_texture(char *key)
 {
@@ -64,6 +30,28 @@ int	is_cardinal_direction(char *key)
 	if (ft_strncmp(key, "WE", 2) == 0 && ft_strlen(key) == 2)
 		return (1);
 	if (ft_strncmp(key, "EA", 2) == 0 && ft_strlen(key) == 2)
+		return (1);
+	return (0);
+}
+
+int	is_valid_key(char *key)
+{
+	return (is_cardinal_direction(key) || is_color_texture(key));
+}
+
+int	is_duplicate_key(char *key, t_game *game)
+{
+	if (ft_strncmp(key, "NO", 2) == 0 && game->textures.north)
+		return (1);
+	if (ft_strncmp(key, "SO", 2) == 0 && game->textures.south)
+		return (1);
+	if (ft_strncmp(key, "WE", 2) == 0 && game->textures.west)
+		return (1);
+	if (ft_strncmp(key, "EA", 2) == 0 && game->textures.east)
+		return (1);
+	if (ft_strncmp(key, "F", 1) == 0 && game->textures.floor != -1)
+		return (1);
+	if (ft_strncmp(key, "C", 1) == 0 && game->textures.ceiling != -1)
 		return (1);
 	return (0);
 }
