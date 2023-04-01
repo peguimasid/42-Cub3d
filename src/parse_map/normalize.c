9fe9d8e0083646274d360a7642101aa27c6ddbd4
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-01 20:00:58 -0300

    improving code

--- src/parse_map/normalize.c
+++ src/parse_map/normalize.c
@@ -6,42 +6,44 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 21:39:32 by gmasid            #+#    #+#             */
-/*   Updated: 2023/03/31 21:51:13 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/04/01 20:00:48 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-void	normalize_row(char *row, int row_index, t_game *game)
+char	*normalize_row(int row_index, t_game *game)
 {
-	int	i;
+	char	*result;
+	int		i;
 
+	result = malloc(sizeof(char) * (game->map.width + 1));
 	i = 0;
 	while (i < game->map.width)
 	{
 		if (i >= (int)ft_strlen(game->map.array[row_index]))
-			row[i] = ' ';
+			result[i] = ' ';
 		else
-			row[i] = game->map.array[row_index][i];
+			result[i] = game->map.array[row_index][i];
 		i++;
 	}
-	row[i] = '\0';
+	result[i] = '\0';
+	return (result);
 }
 
 void	normalize_map(t_game *game)
 {
-	char	**temp;
+	char	**result;
 	int		i;
 
-	temp = malloc(sizeof(char *) * (game->map.height + 1));
+	result = malloc(sizeof(char *) * (game->map.height + 1));
 	i = 0;
 	while (i < game->map.height)
 	{
-		temp[i] = malloc(sizeof(char) * (game->map.width + 1));
-		normalize_row(temp[i], i, game);
+		result[i] = normalize_row(i, game);
 		i++;
 	}
-	temp[i] = NULL;
+	result[i] = NULL;
 	free_matrix(game->map.array);
-	game->map.array = temp;
+	game->map.array = result;
 }
