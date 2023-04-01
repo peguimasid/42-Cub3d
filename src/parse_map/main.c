0a2f7004d4d7a2610d751472c6b0e7db6476f8f0
Guilhermo Masid <guilhermomasid@gmail.com>
2023-03-31 21:32:33 -0300

    normalize map and handle empty map

--- src/parse_map/main.c
+++ src/parse_map/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:19:23 by gmasid            #+#    #+#             */
-/*   Updated: 2023/03/31 20:46:52 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/03/31 21:30:09 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,37 @@ void	parse_map_file_and_set_textures(char *path, t_game *game)
 		free(line);
 		line = get_next_line_trimmed(fd);
 	}
+	if (!matrix_len(game->map.array))
+		exit_app("Map is empty", game);
 	close(fd);
 }
 
 void	normalize_map(t_game *game)
 {
-	(void)game;
+	char	**temp;
+	int		i;
+	int		j;
+
+	temp = malloc(sizeof(char *) * (game->map.height + 1));
+	i = 0;
+	while (i < game->map.height)
+	{
+		j = 0;
+		temp[i] = malloc(sizeof(char) * (game->map.width + 1));
+		while (j < game->map.width)
+		{
+			if (j >= (int)ft_strlen(game->map.array[i]))
+				temp[i][j] = ' ';
+			else
+				temp[i][j] = game->map.array[i][j];
+			j++;
+		}
+		temp[i][j] = '\0';
+		i++;
+	}
+	temp[i] = NULL;
+	free_matrix(game->map.array);
+	game->map.array = temp;
 }
 
 void	parse_map(char *path, t_game *game)
