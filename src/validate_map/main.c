f7b7e81a28c969c5759a12d73e1e30997e5c18b1
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-03 19:19:13 -0300

    chenge check order

--- src/validate_map/main.c
+++ src/validate_map/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 11:56:04 by gmasid            #+#    #+#             */
-/*   Updated: 2023/04/03 19:11:07 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/04/03 19:18:33 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	validate_map(t_game *game)
 {
-	check_player_existence(game);
 	check_invalid_char(game);
+	check_player_existence(game);
 }
