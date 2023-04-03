0d10ca8c15ae3d2cd43b091dbda86786b2af4f11
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-03 18:34:44 -0300

    check if map has only valid chars

--- src/validate_map/main.c
+++ src/validate_map/main.c
@@ -6,25 +6,34 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 11:56:04 by gmasid            #+#    #+#             */
-/*   Updated: 2023/04/02 11:57:52 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/04/03 18:33:28 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-void	print_map(char **matrix)
+int	contains_invalid_char(t_game *game)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while (matrix && matrix[i])
+	while (game->map.array[i])
 	{
-		printf("'%s'\n", matrix[i]);
+		j = 0;
+		while (game->map.array[i][j])
+		{
+			if (!ft_strchr("NSEW 01", game->map.array[i][j]))
+				return (1);
+			j++;
+		}
 		i++;
 	}
+	return (0);
 }
 
 void	validate_map(t_game *game)
 {
-	print_map(game->map.array);
+	if (contains_invalid_char(game))
+		exit_app("You provide invalid char in map", game);
 }
