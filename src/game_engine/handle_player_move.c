9fd0a10ef6d2f19c39ef0876cecf934575894d45
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-08 13:31:39 -0300

    improing code

--- src/game_engine/handle_player_move.c
+++ src/game_engine/handle_player_move.c
@@ -6,24 +6,47 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 18:08:15 by gmasid            #+#    #+#             */
-/*   Updated: 2023/04/08 13:20:05 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/04/08 13:29:36 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
+float	get_angle(int key, t_game *game)
+{
+	if (key == KEY_A)
+		return (game->ray.angle - 90);
+	if (key == KEY_S)
+		return (game->ray.angle - 180);
+	if (key == KEY_D)
+		return (game->ray.angle + 90);
+	return (game->ray.angle);
+}
+
+void	move_player(int key, t_game *game, float ray_cos, float ray_sin)
+{
+	float	angle;
+
+	angle = get_angle(key, game);
+	ray_cos = cos(degree_to_radians(angle)) * game->player.speed;
+	ray_sin = sin(degree_to_radians(angle)) * game->player.speed;
+	game->player.y_pos += ray_sin;
+	game->player.x_pos += ray_cos;
+}
+
 void	handle_player_move(t_game *game)
 {
+	printf(">>> %f %f\n", game->player.x_pos, game->player.y_pos);
 	if (game->keys.is_arrow_left_pressed)
 		game->ray.angle -= 3;
 	if (game->keys.is_arrow_right_pressed)
 		game->ray.angle += 3;
 	if (game->keys.is_w_pressed)
-		printf("User is pressing W\n");
+		move_player(KEY_W, game, 0, 0);
 	if (game->keys.is_a_pressed)
-		printf("User is pressing A\n");
+		move_player(KEY_A, game, 0, 0);
 	if (game->keys.is_s_pressed)
-		printf("User is pressing S\n");
+		move_player(KEY_S, game, 0, 0);
 	if (game->keys.is_d_pressed)
-		printf("User is pressing D\n");
+		move_player(KEY_D, game, 0, 0);
 }
