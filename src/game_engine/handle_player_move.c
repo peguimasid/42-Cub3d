0d42ac9c6d03218a1d5cf3b3fb77af94c3a4c2c8
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-08 13:20:36 -0300

    creating ifs to handle key press

--- src/game_engine/handle_player_move.c
+++ src/game_engine/handle_player_move.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 18:08:15 by gmasid            #+#    #+#             */
-/*   Updated: 2023/04/05 18:08:57 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/04/08 13:20:05 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	handle_player_move(t_game *game)
 {
-	(void)game;
+	if (game->keys.is_arrow_left_pressed)
+		game->ray.angle -= 3;
+	if (game->keys.is_arrow_right_pressed)
+		game->ray.angle += 3;
+	if (game->keys.is_w_pressed)
+		printf("User is pressing W\n");
+	if (game->keys.is_a_pressed)
+		printf("User is pressing A\n");
+	if (game->keys.is_s_pressed)
+		printf("User is pressing S\n");
+	if (game->keys.is_d_pressed)
+		printf("User is pressing D\n");
 }
