c3fa939296f5546b163681eadd27d6c3f891c7cc
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-16 11:26:40 -0300

    remove unnecessary code

--- src/game_engine/handle_texture.c
+++ src/game_engine/handle_texture.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 15:15:37 by gmasid            #+#    #+#             */
-/*   Updated: 2023/04/14 15:21:04 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/04/16 11:26:13 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	handle_texture(int x, int y, int wall_height, t_game *game)
 {
-	(void)x;
-	(void)y;
 	(void)wall_height;
-	(void)game;
 	return (set_pixel_color(x, y, 0xFFFF00, game));
 }
