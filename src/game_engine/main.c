89fe891cd08d3a29f5e5d83f9f0c57da1fd9051f
Guilhermo Masid <guilhermomasid@gmail.com>
2023-04-18 12:35:58 -0300

    fix: east and west directions swapped

--- src/game_engine/main.c
+++ src/game_engine/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 18:02:59 by gmasid            #+#    #+#             */
-/*   Updated: 2023/04/17 18:50:46 by gmasid           ###   ########.fr       */
+/*   Updated: 2023/04/18 12:35:23 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ void	initialize_window(t_game *game)
 
 void	initialize_ray(t_game *game)
 {
-	if (game->player.dir == 'N')
-		game->ray.angle = 180;
-	if (game->player.dir == 'W')
-		game->ray.angle = 90;
 	if (game->player.dir == 'S')
 		game->ray.angle = 0;
 	if (game->player.dir == 'E')
+		game->ray.angle = 90;
+	if (game->player.dir == 'N')
+		game->ray.angle = 180;
+	if (game->player.dir == 'W')
 		game->ray.angle = 270;
 }
 
